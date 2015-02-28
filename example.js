48eb38cea4efd3dcbab83a7fec8d0a1423954d53
henning96 <ger.henning96@gmail.com>
2015-02-28 15:18:46 +0100

    Added documentation

--- example.js
+++ example.js
@@ -1,16 +1,21 @@
+//init
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var ExpressWAF = require('./express-waf').ExpressWAF;
 
-var mongoDBWrapper = require('./express-waf').MongoDBWrapper;
-var wrapper = new mongoDBWrapper('localhost', 27017, 'block', 'blocklist');
+var EmulatedDB = require('./../database/emulated-db');
+var emudb = new EmulatedDB();
+
+//construct firewall with configuration options
+//at least the database and the blacklist timeout have to be set
 var waf = new ExpressWAF({
-    db: wrapper,
+    db: emudb,
     blockTime: 10000
 });
 
-
+//add modules to the firewall
+//name and configuration for the specific module have to be set
 waf.addModule('xss-module', {}, function(error) {
     console.log(error);
 });
@@ -26,31 +31,21 @@ waf.addModule('csrf-module', {
     refererIndependentUrls: ['/']
 }, function (error) {
     console.log(error);
-})
+});
 
+//body parser is necessary for some modules
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
+//add the configured firewall to your express environment
 app.use(waf.check);
 app.use(express.static("./public"));
 
+//some example routes
 app.get('/', function(req, res) {
     res.status(200).end('Hello world!');
 });
-app.delete('/', function(req, res) {
-    res.status(200).end('Hello world!');
-});
-app.post('/', function(req, res) {
-    res.status(200).end('Hello world!');
-});
-app.put('/', function(req, res) {
-    res.status(200).end('Hello world!');
-});
-app.get('/users', function (req, res) {
-    res.status(200).end('Hello world!');
-})
 
 app.listen(8080);
