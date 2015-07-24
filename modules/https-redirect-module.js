42770c0cc0d58873ea05769e85211c37bb909dcd
yann.hasselmann <yann.hasselmann@tomm-apps.de>
2015-07-24 15:44:28 +0200

    removed dev comments

--- modules/https-redirect-module.js
+++ modules/https-redirect-module.js
@@ -12,22 +12,18 @@
 
     https_redirect.prototype.check = function(req, res, next) {
 
-        // TODO check for openshift headers
-        var is_open_shift = req.headers.hasOwnProperty("x-forwarded-proto");
+        var has_proxy_header = req.headers.hasOwnProperty("x-forwarded-proto");
 
-
-        if (is_open_shift) {
-            // TODO redirect to openShift https
+        if (has_proxy_header) {
             if (req.headers["x-forwarded-proto"] !== "https") {
                 res.redirect("https://" + req.headers.host + req.url);
             } else {
                 next();
             }
         } else {
-            // TODO check if is https
+            // 
             var is_https_req = req.secure;
             if (is_https_req) {
-                // Do nothing
                 next();
             } else {
                 res.redirect("https://" + req.headers.host + req.url);
